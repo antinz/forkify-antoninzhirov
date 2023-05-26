61255304559fb10d7a7c4ac7f7e066feed6e467d
antinz <antoninzhirov@gmail.com>
2023-05-26 14:28:01 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = '94c5fae2-8b29-4332-bd4f-caf41a0c2672';
